3985463c04818bbd518bd7fd25999acc52acc0a5
Nico Coetzee <nicc777@gmail.com>
2023-04-03 10:11:43 +0000

    feat: retries of exception wrapper

--- src/mantellum/decorators.py
+++ src/mantellum/decorators.py
@@ -22,24 +22,47 @@ def timer(func):
     return wrapper_timer
 
 
-def retry_on_exception(number_of_retries: int=3, default_after_all_retries_failed=raise_general_exception(description='All retries exhausted'), sleep_time_seconds_between_retries: int=1):
-    def retry_func(func):
-        l = get_logger()
-        @functools.wraps(func)
-        def wrapper_func(*args, **kwargs):
-            retries = 0
-            while retries < number_of_retries+1:
-                l.info('Try #{} for function {}()'.format(retries, func.__name__))
-                try:
-                    retries += 1
-                    value = func(*args, **kwargs)
-                    return Value
-                except:
-                    l.error('EXCEPTION: {}'.format(traceback.format_exc()))
-                    time.sleep(sleep_time_seconds_between_retries)
-            l.info('Function {}() retried {} times without success'.format(func.__name__, retries))
-            if isinstance(default_after_all_retries_failed, Exception):
-                raise default_after_all_retries_failed
-            return default_after_all_retries_failed
-        return wrapper_func
+def retry_on_exception(number_of_retries: int=3, default_after_all_retries_failed=None, sleep_time_seconds_between_retries: int=1):
+    """
+    Example usage (Forcing a failure):
+
+        >>> from mantellum.decorators import *
+        >>> @retry_on_exception(number_of_retries=4, default_after_all_retries_failed='It Failed!')
+        ... def F():
+        ...     raise Exception('This will never work!')
+        ... 
+        >>> F()
+        'It Failed!'
+        >>> 
+
+    Example usage (Normal usage with defaults):
+        >>> from mantellum.decorators import *
+        >>> @retry_on_exception()
+        ... def F():
+        ...     return 123
+        ... 
+        >>> F()
+        123
+    """
+    exception_thrown = False
+    try:
+        def retry_func(func):
+            l = get_logger()
+            @functools.wraps(func)
+            def wrapper_func(*args, **kwargs):
+                retries = 0
+                while retries < number_of_retries+1:
+                    l.info('Try #{} for function {}()'.format(retries, func.__name__))
+                    try:
+                        retries += 1
+                        value = func(*args, **kwargs)
+                        return value
+                    except:
+                        l.error('EXCEPTION: {}'.format(traceback.format_exc()))
+                        time.sleep(sleep_time_seconds_between_retries)
+                l.info('Function {}() retried {} times without success'.format(func.__name__, retries-1))
+                raise Exception('All retries failed')
+            return wrapper_func
+    except:
+        exception_thrown = True
     return retry_func
